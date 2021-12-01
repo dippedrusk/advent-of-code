ab9351cb9e393161b7e7858d3cf9beef84d9680c
vgautam <vasundhara1997@gmail.com>
2021-12-01 13:50:15 +0100

    clean up 2021 day 1

--- 2021/01.py
+++ 2021/01.py
@@ -1,5 +1,5 @@
 with open('input01', 'r', encoding='utf-8') as f:
-    xs = f.read().strip().splitlines()
+    xs = [int(x) for x in f.read().strip().splitlines()]
 
 """
 xs = '''199
@@ -15,21 +15,17 @@ xs = '''199
 '''.splitlines()
 """
 
-xs = [int(x) for x in xs]
-
 count = 0
 for (x,y) in zip(xs, xs[1:]):
     if y > x:
         count += 1
 
-print(f'part1: {count}')
+print(f'part1: {count}') # 2 mins 56 seconds
 
 count = 0
 for i, (a,b,c) in enumerate(zip(xs[1:], xs[2:], xs[3:])):
     x, y, z = xs[i], xs[i+1], xs[i+2]
-    if sum([int(a),int(b),int(c)]) > sum([int(x),int(y),int(z)]):
+    if sum([a,b,c]) > sum([x,y,z]):
         count += 1
 
-print(f'part2: {count}')
-
-# ~ 8 mins
+print(f'part2: {count}') # 8 mins 15 seconds including part 1
