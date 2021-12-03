263447a91358bb87a696635b70903ebe50db6e88
vgautam <vasundhara1997@gmail.com>
2021-12-03 06:58:08 +0100

    add 2021 day 3

---
+++ 2021/03.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from collections import Counter
+
+with open('input03', 'r', encoding='utf-8') as f:
+    xs = f.read().strip().splitlines()
+
+ps = defaultdict(list)
+
+for x in xs:
+    for i,c in enumerate(x):
+        ps[i].append(int(c))
+
+max_ = ''
+min_ = ''
+
+for i in ps:
+    c = Counter(ps[i])
+    max_ += str(max(c, key=c.get))
+    min_ += str(min(c, key=c.get))
+
+print(f'part1: {int(max_, 2) * int(min_, 2)}')
+
+pss = []
+for x in xs:
+    pss.append([int(c) for c in x])
+
+def apply_func(pss, func):
+    for i in range(len(pss[0])):
+        if len(pss) <= 1:
+            break
+        c = Counter([ps[i] for ps in pss])
+        max_ = max(c, key=c.get)
+        min_ = min(c, key=c.get)
+        if max_ == min_:
+            max_ = 1
+            min_ = 0
+        compare = max_ if func == 'max' else min_
+        pss = [ps for ps in pss if ps[i] == compare]
+    return ''.join([str(c) for c in pss[0]])
+
+max_ = apply_func(pss, 'max')
+min_ = apply_func(pss, 'min')
+
+print(f'part2: {int(max_, 2) * int(min_, 2)}')
